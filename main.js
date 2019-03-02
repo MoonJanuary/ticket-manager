5faffcbe10a68229fe459ac6bb677c87f7a683ed
MoonJanuary <tshyk_takahashi@yahoo.co.jp>
2019-03-02 16:02:02 +0900

    最小構成

---
+++ main.js
@@ -0,0 +1,98 @@
+'use strict';
+
+// 定数
+const HTML_DETAIL = '<tr class="detail"><td></td><td></td><td></td><td></td><td></td><td></td></tr>';
+
+// グローバル変数
+let list = [];       // チケット情報のリスト
+let cntTickets = 0;  // チケット数
+let cntClosed = 0;   // 完了数
+let progress = 0;    // 進捗(パーセンテージ、小数点以下切り捨て)
+
+// 要素の参照を取得
+const elTicket = document.querySelector('#tickets');
+const tbdyDetails = document.querySelector('#detailsBody');
+const elResult = document.querySelector('#result');
+const btnOutputResult = document.querySelector('#btnOutputResult');
+
+
+// ファイル選択ダイアログからファイルが選択された場合の処理
+elTicket.addEventListener('change', (event) => {
+  const target = event.target;
+  const files = target.files;
+  
+  // 選択したファイル名からチケット情報のリストを作成(IDの昇順でソート)
+  // TODO:ソートは数値以外が含まれても対応できるように考慮する。
+  for (let file of files) {
+    const data = file.name.replace('.txt', '').split('_');
+    list.push({ id: data[0], issueDay: data[1], closingDay: data[2], issuer: data[3], worker: data[4], title: data[5] });
+  }
+  list.sort((a, b) => {return a.id - b.id;});
+  
+  // チケット情報のリストからテーブルを生成
+  let i = 1;
+  for (let detail of list) {
+  
+    // 各セルに値を設定
+    tbdyDetails.insertAdjacentHTML('beforeend', HTML_DETAIL);
+    tbdyDetails.rows[i].cells[0].appendChild(document.createTextNode(detail.id));
+    tbdyDetails.rows[i].cells[1].appendChild(document.createTextNode(detail.issueDay));
+    tbdyDetails.rows[i].cells[2].appendChild(document.createTextNode(detail.closingDay));
+    tbdyDetails.rows[i].cells[3].appendChild(document.createTextNode(detail.issuer));
+    tbdyDetails.rows[i].cells[4].appendChild(document.createTextNode(detail.worker));
+    tbdyDetails.rows[i].cells[5].appendChild(document.createTextNode(detail.title));
+    i++;
+    
+    // 完了したチケット数をカウント
+    if (detail.closingDay !== '00000000') {
+      cntClosed++;
+    }
+  }
+  
+  // チケット数を取得
+  cntTickets = list.length;
+  // 進捗率を算出
+  // TODO:誤差の無い計算を理解して適するなら取り入れる。
+  progress = Math.trunc(cntClosed / cntTickets * 100);
+  
+  // 集計結果を出力
+  elResult.innerHTML = 'チケット数:' + cntTickets + '&nbsp完了数:' + cntClosed + '&nbsp進捗率:' + progress + '%';
+});
+
+
+// 集計結果出力ボタン押下時の処理
+btnOutputResult.addEventListener('click', () => {
+  // チケットのリストをファイル出力用の文字列に編集
+  let strBuffer = '';
+  for (let detail of list) {
+    strBuffer += detail.id + '\t' + detail.issueDay + '\t' + detail.closingDay + '\t' + detail.issuer + '\t' + detail.worker + '\t' + detail.title + '\n';
+  }
+  
+  // ファイル名を生成
+  const date = new Date();
+  const yyyy = date.getFullYear();
+  const mm = ('00' + (date.getMonth()+1)).slice(-2);
+  const dd = ('00' + date.getDate()).slice(-2);
+  const outFileName = yyyy + mm + dd + '_' + cntTickets + '_' + cntClosed + '_' + progress + '.txt';
+  
+  // テキストをBLOBとして生成
+  const blResultFile = new Blob([strBuffer], {type:'text/plain'});
+  
+  // ブラウザによりファイル出力処理を切り替え
+  if (window.navigator.msSaveBlob) {
+    // IE
+    window.navigator.msSaveBlob(blResultFile, outFileName);
+  } else if (window.URL && window.URL.createObjectURL) {
+    // FireFox、Chrome、Safari
+    const a = document.createElement('a');
+    a.download = outFileName;
+    a.href = window.URL.createObjectURL(blResultFile);
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  } else {
+    // その他
+    const base64ResultFile = 'data:text/plain,' + encodeURIComponent(elResult.textContent);
+    window.open(base64ResultFile, '_blank');
+  }
+});
